1fe1dfc61faa60385baa1e42586eab41982d5bf2
LIBERCOSOFT <akinspreciousola@gmail.com>
2023-02-27 08:48:31 +0100

    Add username field to user table

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ApplicationRecord
   include Devise::JWT::RevocationStrategies::JTIMatcher
   devise :database_authenticatable, :registerable, :jwt_authenticatable, jwt_revocation_strategy: self
 
+  validates :email, presence: true
+  validates :username, presence: true
+
   def jwt_payload
     super
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_25_125415) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_27_073009) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,6 +23,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_25_125415) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "jti", null: false
+    t.string "username", default: "", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["jti"], name: "index_users_on_jti", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
