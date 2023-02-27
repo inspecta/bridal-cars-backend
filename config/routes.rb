53f97bcf43217d2d86e45670a4b8125fb469fb14
Derrick Mulinde <djmulinde@gmail.com>
2023-02-27 12:23:28 +0300

    Add car api routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
+  # car api endpoints
+  # post '/api/v1/', to: 'api/v1#create'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      resources :cars, only: [:index, :show, :create, :destroy]
+    end
+  end
+
 end
