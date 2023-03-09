a8a2f7de654e873606121a57e3a787237351165b
Derrick Mulinde <djmulinde@gmail.com>
2023-03-09 20:19:13 +0300

    Fix deploy errors

--- config/application.rb
+++ config/application.rb
@@ -1,12 +1,11 @@
 require_relative "boot"
 
 require "rails/all"
+require "sprockets/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-# Bundler.require(*Rails.groups)
-Bundler.require(:default, :assets, Rails.env)
-
+Bundler.require(*Rails.groups)
 
 module CarApi
   class Application < Rails::Application
