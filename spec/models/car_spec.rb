9e4ab4530d0a0accd267e3032be1ea81bf6a77a3
LIBERCOSOFT <akinspreciousola@gmail.com>
2023-03-09 14:45:42 +0100

    Fix linter errors

--- spec/models/car_spec.rb
+++ spec/models/car_spec.rb
@@ -3,17 +3,17 @@ require 'rails_helper'
 RSpec.describe Car, type: :model do
   it 'should be valid' do
     car = Car.create(name: '',
-    model: '',
-    price: 0,
-    description: '',)
+                     model: '',
+                     price: 0,
+                     description: '')
     expect(car).to_not be_valid
   end
 
   it 'should create a new car' do
     car = Car.create(name: 'Car Name',
-    model: 'M1',
-    price: 0,
-    description: 'very good car',)
+                     model: 'M1',
+                     price: 0,
+                     description: 'very good car')
     expect(car.name).to eql('Car Name')
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,15 +3,15 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   it 'should be valid' do
     user = User.create(username: '',
-    email: '',
-    password: '')
+                       email: '',
+                       password: '')
     expect(user).to_not be_valid
   end
 
   it 'should create a new user' do
     user = User.create(username: 'user 1',
-    email: 'useremail@email.com',
-    password: 'password')
+                       email: 'useremail@email.com',
+                       password: 'password')
     expect(user.username).to eql('user 1')
   end
 end
